d04495688773082a0ef11409cc7ba0b6408b3d5a
AleksandrKriukov <alexanderkry@gmail.com>
2018-02-24 22:40:08 +0300

    added 2 and 3

--- TDDFeb2018/src/RomanNumerals/RomanNumerals.java
+++ TDDFeb2018/src/RomanNumerals/RomanNumerals.java
@@ -3,6 +3,10 @@ package RomanNumerals;
 public class RomanNumerals {
 
     public static String convertToRoman(int arabic) {
-        return "I";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arabic; i++) {
+            sb.append("I");
+        }
+        return sb.toString();
     }
 }
--- TDDFeb2018/test/RomanNumerals/RomanNumeralsTest.java
+++ TDDFeb2018/test/RomanNumerals/RomanNumeralsTest.java
@@ -1,15 +1,25 @@
 package RomanNumerals;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RomanNumeralsTest {
 
+    @Before
+    RomanNumerals rn = new RomanNumerals();
+
     @Test
-    public void one() {
-        RomanNumerals rn = new RomanNumerals();
+    public void oneTwoThree() {
         Assert.assertEquals("I", rn.convertToRoman(1));
+        Assert.assertEquals("II", rn.convertToRoman(2));
+        Assert.assertEquals("III", rn.convertToRoman(3));
+    }
+
+    @Test
+    public void four() {
+
     }
 }
