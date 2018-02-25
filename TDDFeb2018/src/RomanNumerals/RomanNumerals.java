d7956f39d4cf97bff84822a59dbadb19a8ce69b7
AleksandrKriukov <alexanderkry@gmail.com>
2018-02-25 12:58:22 +0300

    converter to roman for 9 done

--- TDDFeb2018/src/RomanNumerals/RomanNumerals.java
+++ TDDFeb2018/src/RomanNumerals/RomanNumerals.java
@@ -2,11 +2,24 @@ package RomanNumerals;
 
 public class RomanNumerals {
 
+    public static final int[] VALUES = {9, 5 , 4, 1};
+    public static final String[] SYMBOLS = {"IX", "V" , "IV", "I"};
     public static String convertToRoman(int arabic) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arabic; i++) {
-            sb.append("I");
+        int remaining = arabic;
+
+        for (int i = 0; i < VALUES.length; i++) {
+            remaining = convert(sb, VALUES[i], SYMBOLS[i], remaining);
         }
+
         return sb.toString();
+    }
+
+    private static int convert(StringBuilder sb, int value, String roman, int remaining) {
+        while (remaining >= value) {
+            sb.append(roman);
+            remaining -= value;
+        }
+        return remaining;
     }
 }
--- TDDFeb2018/test/RomanNumerals/RomanNumeralsTest.java
+++ TDDFeb2018/test/RomanNumerals/RomanNumeralsTest.java
@@ -1,5 +1,6 @@
 package RomanNumerals;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,7 @@ import static org.junit.Assert.*;
 
 public class RomanNumeralsTest {
     private RomanNumerals rn;
+
     @Before
     public void setup() {
         rn = new RomanNumerals();
@@ -23,5 +25,23 @@ public class RomanNumeralsTest {
     @Test
     public void four() {
         Assert.assertEquals("IV", rn.convertToRoman(4));
+    }
+
+    @Test
+    public void fiveSixSevenEight() {
+        Assert.assertEquals("V", rn.convertToRoman(5));
+        Assert.assertEquals("VI", rn.convertToRoman(6));
+        Assert.assertEquals("VII", rn.convertToRoman(7));
+        Assert.assertEquals("VIII", rn.convertToRoman(8));
+    }
+
+    @Test
+    public void nine() {
+        Assert.assertEquals("IX", rn.convertToRoman(9));
+    }
+
+    @After
+    public void cleanup() {
+        rn = null;
     }
 }
