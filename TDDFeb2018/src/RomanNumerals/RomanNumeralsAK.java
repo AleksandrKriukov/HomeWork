bd6cd05e25918bc7b0bb89d8f01ea00ca3ab2c3f
AleksandrKriukov <alexanderkry@gmail.com>
2018-02-25 23:30:10 +0300

    arabicToRoman done

--- TDDFeb2018/src/RomanNumerals/RomanNumeralsAK.java
+++ TDDFeb2018/src/RomanNumerals/RomanNumeralsAK.java
@@ -2,12 +2,23 @@ package RomanNumerals;
 
 public class RomanNumeralsAK {
 
+    private final static int[] NUMBERS = {10, 9, 5, 4, 1};
+    private final static String[] SYMBOLS = {"X", "IX", "V", "IV", "I"};
+
     public static String arabicToRoman(int arabic) {
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < arabic; i++) {
-            sb.append("I");
+        for (int i = 0; i < NUMBERS.length; i++) {
+            arabic = convert(arabic, sb, NUMBERS[i], SYMBOLS[i]);
         }
         return sb.toString();
+    }
+
+    private static int convert(int arabic, StringBuilder sb, int value, String symbol) {
+        while (arabic >= value) {
+            sb.append(symbol);
+            arabic -= value;
+        }
+        return arabic;
     }
 }
